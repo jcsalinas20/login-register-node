125a3b3badce2b48ce54b690b4a582b8c08db7cc
jcsalinas20 <jcsalinasn20@gmail.com>
2021-01-22 18:54:12 +0100

    v2 login with api

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const express = require("express")
+const moment = require('moment');
+const uuidv4 = require('uuid');
+const sha1 = require('sha1');
 const bodyParser = require("body-parser")
 const app = express()
 
@@ -65,6 +68,124 @@ app.post("/register", (req, res) => {
     } else res.redirect('register?type=error&msg=Las contraseñas no son iguales.');
 })
 
+app.post("/api/logout", (req, res) => {
+    var username = req.body.user;
+    var password = req.body.pass;
+    var status = 0;
+    var msg = "";
+
+    let users = JSON.parse(localStorage.getItem('users'));
+    users.map(function (user) {
+        if (user.name === username && user.pass == password) {
+            user.token = '';
+            status = 1;
+            msg = `Hasta otra ${username}.`;
+        }
+    });
+
+
+    if (status) {
+        localStorage.setItem('users', JSON.stringify(users));
+        res.header("Content-Type", "application/json")
+        res.send(
+            JSON.stringify({
+                type: "success",
+                msg: msg
+            }, null, 2)
+        )
+    } else {
+        res.header("Content-Type", "application/json")
+        res.send(
+            JSON.stringify({
+                type: "error",
+                msg: `No se ha encontrado el usuario: ${username}.`
+            }, null, 2)
+        )
+    }
+});
+
+app.post("/api/login", (req, res) => {
+    var username = req.body.user;
+    var password = req.body.pass;
+    var status = 0;
+    var msg = "";
+
+    let users = JSON.parse(localStorage.getItem('users'));
+    users.map(function (user) {
+        if (user.name === username && user.pass == password) {
+            const ORDER_ID = getOrderId()
+            const TIMESTAMP = getMoment()
+            status = 1;
+
+            const token = createHash(
+                ORDER_ID,
+                TIMESTAMP,
+                password
+            )
+            user.token = token;
+
+            msg = `Bienvenido ${username} este es tu token: ${token}.`;
+        }
+    });
+
+
+    if (status) {
+        localStorage.setItem('users', JSON.stringify(users));
+        res.header("Content-Type", "application/json")
+        res.send(
+            JSON.stringify({
+                type: "success",
+                msg: msg
+            }, null, 2)
+        )
+    } else {
+        res.header("Content-Type", "application/json")
+        res.send(
+            JSON.stringify({
+                type: "error",
+                msg: `No se ha encontrado el usuario: ${username}.`
+            }, null, 2)
+        )
+    }
+});
+
+app.post("/api/register", (req, res) => {
+    var username = req.body.user;
+    var password = req.body.pass;
+
+    let users = JSON.parse(localStorage.getItem('users'));
+    let newUser = {
+        name: username,
+        pass: password,
+        token: ''
+    };
+    users.push(newUser);
+
+    localStorage.setItem('users', JSON.stringify(users));
+    res.header("Content-Type", "application/json")
+    res.send(
+        JSON.stringify({
+            type: "success",
+            msg: `El usuario ${username} se ha registrado correctamente.`
+        }, null, 2)
+    )
+});
+
 app.listen("1337", async () => {
     console.log("API REST corriendo en el puerto 1337");
 });
+
+
+function createHash(...params) {
+    const token = sha1(params.join("."))
+
+    return token;
+}
+
+function getOrderId() {
+    return uuidv4.v4();
+}
+
+function getMoment() {
+    return moment().format("YYYYMMDDHHmmss");
+}
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Test CURL
+
+## End point '/api/register'
+``` curl -X POST -H "Content-Type: application/json" -d '{"user":"new_user","pass":"password"}'  http://localhost:1337/api/register; ```
